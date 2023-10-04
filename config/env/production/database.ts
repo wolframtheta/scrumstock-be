f9eadf063ea3d97aea303300d968c7e0aad2ae5b
Xavier Marques <xaviermarques4f@gmail.com>
2023-10-04 08:45:15 +0200

    Change prod

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -15,17 +15,6 @@ export default ({ env }) => ({
         rejectUnauthorized: false
       },
     },
-    acquireConnectionTimeout: 1000000,
-    pool: {
-      min: 0,
-      max: 4,
-      acquireTimeoutMillis: 300000,
-      createTimeoutMillis: 300000,
-      destroyTimeoutMillis: 300000,
-      idleTimeoutMillis: 30000,
-      reapIntervalMillis:1000,
-      createRetryIntervalMillis: 2000
-    },
     debug: false,
   },
 });
