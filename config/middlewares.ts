095daa86bd42d6eb7740a58f6b36ef27a5f15fb4
Xavier Marques <xaviermarques4f@gmail.com>
2023-10-06 20:21:32 +0200

    Add cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,21 +1,22 @@
-export default [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+// export default [
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::logger',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
 
 module.exports = {
   //...
   settings: {
     cors: {
-      origin: ['*'], //allow all
+      origin: ['*'], //allow all origins
+      headers: ['*'], //allow all headers
     },
   },
 };
